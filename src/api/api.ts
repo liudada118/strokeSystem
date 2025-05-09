d2ebd24d9ed18bca8795fa3898d10d8495eb775d
yuyongwei1 <17634782013@163.com>
2025-05-09 10:12:08 +0800

    版本好2.0 4

--- src/api/api.ts
+++ src/api/api.ts
@@ -8,9 +8,9 @@ export const netUrl = `http://${web}:8080/rcv`;
 export const netRepUrl = `http://${web}:8081`;
 export const voiceUrl = `http://${web}`
 
-// export const netUrl = `https://${web}/rcv`;
-// export const netRepUrl = `https://${web}`;
-// export const voiceUrl = `https://${web}`
+export const netUrl = `https://${web}/rcv`;
+export const netRepUrl = `https://${web}`;
+export const voiceUrl = `https://${web}`
 
 // export const netUrl = "https://sensor.bodyta.com/rcv";
 // export const netRepUrl = "https://sensor.bodyta.com";

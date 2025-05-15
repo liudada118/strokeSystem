a1b980654ca5e366ff3e4a748bbd5e4e78a05c13
liudada118 <62827155+liudada118@users.noreply.github.com>
2025-05-15 18:22:38 +0800

    结合android解决语音
    结合android解决语音

--- src/redux/Middleware/constant.ts
+++ src/redux/Middleware/constant.ts
@@ -1,28 +1,30 @@
+import { message } from "antd";
+
 // export const HOST = 'sensor.bodyta.com'
 export const HOST = 'juqiao.bodyta.com'
 export const PORT = 443
 
 const textMap: { [key: string]: string } = {
-    '0' : '零',
-    '1' : '一',
-    '2' : '二',
-    '3' : '三',
-    '4' : '四',
-    '5' : '五',
-    '6' : '六',
-    '7' : '七',
-    '8' : '八',
-    '9' : '九',
-    '-' : '杠',
-    '_' : '杠'
+    '0': '零',
+    '1': '一',
+    '2': '二',
+    '3': '三',
+    '4': '四',
+    '5': '五',
+    '6': '六',
+    '7': '七',
+    '8': '八',
+    '9': '九',
+    '-': '杠',
+    '_': '杠'
 };
 
-export function exChangeText(str : string){
-    
-    let strArr = str.split('').map((a : string) => {
-        if(Object.keys(textMap).includes(a)){
+export function exChangeText(str: string) {
+
+    let strArr = str.split('').map((a: string) => {
+        if (Object.keys(textMap).includes(a)) {
             return textMap[a]
-        }else{
+        } else {
             return a
         }
     })
@@ -30,11 +32,35 @@ export function exChangeText(str : string){
     return strArr.join('')
 }
 
-export function reloadWebview(){
+export function reloadWebview() {
     try {
         const u = window.navigator.userAgent
         if (u.indexOf('Android') > -1 || u.indexOf('Adr') > -1) {
             eval(`Android.reloadWebView();`)
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export function audioPlay() {
+    message.info('audioPlay')
+    try {
+        const u = window.navigator.userAgent
+        if (u.indexOf('Android') > -1 || u.indexOf('Adr') > -1) {
+            eval(`Android.audioPlay();`)
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export function audioPause(){
+    message.info('audioPause')
+    try {
+        const u = window.navigator.userAgent
+        if (u.indexOf('Android') > -1 || u.indexOf('Adr') > -1) {
+            eval(`Android.audioPause();`)
         }
     } catch (err) {
         console.log(err)
